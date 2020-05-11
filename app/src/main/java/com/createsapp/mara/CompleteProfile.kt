386ffff1f8feb94ae9b6b8b3d671a0d8bbefa90b
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-05-12 00:54:53 +0530

    add image selection and set in imageview

--- app/src/main/java/com/createsapp/mara/CompleteProfile.kt
+++ app/src/main/java/com/createsapp/mara/CompleteProfile.kt
@@ -1,14 +1,15 @@
 package com.createsapp.mara
 
 import android.Manifest
+import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_complete_profile.*
-import java.text.SimpleDateFormat
 import java.util.*
 
 class CompleteProfile : AppCompatActivity() {
@@ -19,6 +20,11 @@ class CompleteProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complete_profile)
 
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
         //photo image click
         photoImg.setOnClickListener {
             checkPermissionForImage()
@@ -26,43 +32,78 @@ class CompleteProfile : AppCompatActivity() {
 
         dobBtn.setOnClickListener {
 
-            var cal = Calendar.getInstance()
+            val dpd = DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+                    dobBtn.setText("" + mDay + "/" + mMonth + "/" + mYear)
+                },
+                year,
+                month,
+                day
+            )
 
-            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                cal.set(Calendar.YEAR, year)
-                cal.set(Calendar.MONTH, monthOfYear)
-                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-
-                val myFormat = "dd.MM.yyyy" // mention the format you need
-                val sdf = SimpleDateFormat(myFormat, Locale.US)
-
-                dobBtn.text = sdf.format(cal.time)
-            }
-
+            dpd.show()
         }
+
     }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICKER_CODE = 1000;
+
+        //Permission code
+        private val PERMISSION_CODE = 1001;
+    }
+
 
     private fun checkPermissionForImage() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if ((checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) && (checkSelfPermission(
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ) == PackageManager.PERMISSION_DENIED)
-            ) {
-                val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                val permissionCoarse = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-                requestPermissions(permission, 12)
-                requestPermissions(permissionCoarse, 13)
-
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                //permission denied
+                val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                //show popup to request runtime permission
+                requestPermissions(permission, PERMISSION_CODE);
             } else {
+                //permission already granted
                 pickImageFromGallery()
             }
+        } else {
+            // system OS is >= Marshmallow
+            pickImageFromGallery()
         }
     }
 
     private fun pickImageFromGallery() {
-        var intent = Intent(Intent.ACTION_CHOOSER)
+        var intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICKER)
+        startActivityForResult(intent, IMAGE_PICKER_CODE)
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    //permission from popup granted
+                    pickImageFromGallery()
+                } else {
+                    //permission from popup denied
+                    Toast.makeText(this,"Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICKER_CODE) {
+            photoImg.setImageURI(data!!.data)
+        }
+    }
+
 }
